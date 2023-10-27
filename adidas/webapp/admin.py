d180a399744d895ef7e951730a76115477a25750
Pandora <nimfanefor@gmail.com>
2023-10-27 21:12:03 +0600

    Развернул проект на новой ОС

--- adidas/webapp/admin.py
+++ adidas/webapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from webapp.models import Category, Product, ProductImage
+from webapp.models import Category, Product, ProductImage, Purchase
 
 admin.site.register(Category)
 
@@ -8,3 +8,12 @@ admin.site.register(Product)
 
 admin.site.register(ProductImage)
 
+
+# class PurchaseAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'user', 'product', 'size', 'phone_number', 'delivery_address', 'created_at')
+#     list_filter = ('created_at',)
+#     search_fields = ('user__username', 'product__name', 'phone_number', 'delivery_address')
+#     date_hierarchy = 'created_at'
+#
+#
+# admin.site.register(Purchase, PurchaseAdmin)
